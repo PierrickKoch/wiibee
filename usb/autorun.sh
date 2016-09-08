3bec11fb9ca96b2fed6d71eae947cf350f2513ea
Pierrick Koch <pierrick.koch@gmail.com>
2016-09-08 14:48:23 +0200

    use wiringpi gpio utility

--- usb/autorun.sh
+++ usb/autorun.sh
@@ -9,16 +9,19 @@ BTADDR2="00:22:4c:6e:12:6c"
 
 logger "Simulate press red sync button on the Wii Board"
 
-# TODO use http://wiringpi.com/the-gpio-utility/ instead
-./gpio.py $GPIO1
-./gpio.py $GPIO2
-
-sleep 1
+# http://wiringpi.com/the-gpio-utility/
+gpio mode $GPIO1 out
+gpio mode $GPIO2 out
+gpio write $GPIO1 0
+gpio write $GPIO2 0
+sleep 0.2
+gpio write $GPIO1 1
+gpio write $GPIO2 1
 
 logger "Start listenning to the mass measurements"
 
 pids=""
-# see https://github.com/pierriko/wiiboard
+# https://github.com/pierriko/wiiboard
 ./wiiboard.py $DEBUG BTADDR1 2>> wiiboard1.log >> wiiboard1.txt & pids="$! $pids"
 ./wiiboard.py $DEBUG BTADDR2 2>> wiiboard2.log >> wiiboard2.txt & pids="$! $pids"
 ./temperature.sh >> temperature.txt & pids="$! $pids"
