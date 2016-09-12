e6b996293819d925f72a4a2b32e4abdf899c52be
Pierrick Koch <pierrick.koch@gmail.com>
2016-09-12 14:49:28 +0200

    [index] add dygraph example

--- usb/txt2js.py
+++ usb/txt2js.py
@@ -12,7 +12,5 @@ file.txt must be formated as: "float float\n"*
 import sys
 
 name = sys.argv[1]
-data = [{'x': x, 'y': y} for x, y in \
-        [map(float, line.split()) for line in sys.stdin.readlines()]]
-
+data = [map(float, line.split()) for line in sys.stdin.readlines()]
 print("%s = %r"%(name, data))
