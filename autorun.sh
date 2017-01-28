92435b1e2181a8d01c15becfb1fd381ff5ec8555
Pierrick Koch <pierrick.koch@gmail.com>
2017-01-28 14:21:18 +0000

    [autorun] fix 3g huawei

--- autorun.sh
+++ autorun.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
 # Relay PIN, see: http://pinout.xyz/pinout/wiringpi
-GPIOS="4 5 6" # http://pinout.xyz/pinout/pin16_gpio23
+GPIOS="4 5 6 2" # http://pinout.xyz/pinout/pin16_gpio23
 # Bluetooth MAC, use: hcitool scan, or: python wiiboard.py
-BTADDR="00:1e:35:fd:11:fc 00:22:4c:6e:12:6c 00:1e:35:ff:b0:04"
+BTADDR="00:1e:35:fd:11:fc 00:22:4c:6e:12:6c 00:1e:35:ff:b0:04 00:23:31:84:7E:4C"
+
+# fix Huawei E3135 recognized as CDROM [sr0]
+lsusb | grep 12d1:1f01 && sudo usb_modeswitch -v 0x12d1 -p 0x1f01 -M "55534243123456780000000000000a11062000000000000100000000000000"
+# run DHCP client to get an IP
+ifconfig -a | grep eth1 -A1 | grep inet || sudo dhclient eth1
 
 sleep 12 # FIXME "wait" for dhcpd timeout
 # if BT failed: sudo systemctl status hciuart.service
@@ -40,8 +45,11 @@ logger "Start listenning to the mass measurements"
 python autorun.py $BTADDR >> wiibee.txt
 logger "Stoped listenning"
 python txt2js.py wiibee < wiibee.txt > wiibee.js
-# git commit wiibee.js -m"[data] $(date -Is)"
+# TODO git commit wiibee.* -m"[data] $(date -Is)"
 # TODO git push ssh master
+
+# obexftp -b A0:CB:FD:F7:80:F1 -v -p wiibee.js
+# cp ~/wittyPi/wittyPi.log /mnt/bee1/
 
 [ -z "$WIIBEE_SHUTDOWN" ] && exit 0
 logger "Shutdown WittyPi"
